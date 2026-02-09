80a8067aa3a34dec0548b6c7866355ad9e408c61
Basquiat <jlnmcneal@gmail.com>
2026-02-08 20:45:29 -0600

    Route template setup

---
+++ routing/html/view-notes.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const router = require('express').Router();
+
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/notes', (req, res) => {
+
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routing/api/api-notes');
+const html = require('./routing/html/view-notes')
+
+
+// Instance of the express app (express object);
+const app = express();
+const PORT = 3001;
+
+// Tells express to look in public for client files rendering 
+app.use(express.static('public'));
+
+// Critical when setting up a POST or PUT route. 
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+
+
+ app.use('/api',api);
+ app.use('/', html);
+
+
+app.listen(PORT, () => 
+console.log(`Example app listening at http://localhost:${PORT}`))
