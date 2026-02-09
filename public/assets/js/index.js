e77676778e0078135e39ede59cc7a458e16ef5e6
Basquiat <jlnmcneal@gmail.com>
2026-02-09 00:28:20 -0600

    function to display db notes in html

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -17,12 +17,40 @@ const getRequest = async () => {
 
  const renderNotes = async () => {
   // Fetch request to get db elements
-      const result  = await getRequest();
+      const result  = await getRequest(); 
      console.log(result)
      
+      const noteGroup = document.querySelector('.list-group');
+     noteGroup.innerHTML = "";
+
 // forloop to iterate of db elements
      for( let i = 0; i < result.length; i++ ){
+     const noteCard  = document.createElement("li"); // Create a new <li> element
+     const icon = document.createElement("i");
+     const text = document.createElement("span");
  
+     icon.className = "fas fa-solid text-danger fa-trash";
+     noteCard.className= "list-group-item d-flex justify-content-between align-items-center";
+ 
+     text.textContent = result[i].noteTitle // Create text content
+ 
+     icon.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+       deleteNote(result[i].id);
+       titleEl.value = ""
+       textEl.value = ""
+       
+     });
+     noteCard.addEventListener("click", () => {
+     
+      titleEl.value = result[i].noteTitle
+      textEl.value = result[i].noteText
+     })
+ 
+     noteCard.appendChild(text)
+     noteCard.appendChild(icon)
+     noteGroup.appendChild(noteCard)
      }
  }
 
