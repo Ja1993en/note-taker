03f83d134cb9b1c5621fb8b374ec2eb6c714ec3d
Basquiat <jlnmcneal@gmail.com>
2026-02-08 21:12:12 -0600

    Selected DOM Elements, Added event listeners

---
+++ public/js/index.js
@@ -0,0 +1,39 @@
+const newNoteBtn = document.querySelector('.new-note');
+const saveNoteBtn = document.querySelector('.save-note')
+const titleEl = document.querySelector('.note-title');
+const textEl = document.querySelector('.note-textarea');
+
+
+
+const postNote = () => {
+
+}
+
+
+
+
+const getRequest = async () => {
+
+  
+}
+
+
+  const deleteNote = async (id) => {
+   
+
+  }
+  
+ const renderNotes = async () => {
+   
+ 
+
+}
+
+
+
+
+saveNoteBtn.addEventListener('click', postNote)
+newNoteBtn.addEventListener("click", newNoteHandler);
+textEl.addEventListener("click", saveBtnHandler);
+
+renderNotes();
