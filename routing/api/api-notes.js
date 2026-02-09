ce4428bf59b3917f009d420a17a603f9c607ee8b
Basquiat <jlnmcneal@gmail.com>
2026-02-08 20:56:55 -0600

    Routing for html files

--- routing/api/api-notes.js
+++ routing/api/api-notes.js
@@ -1,12 +1,13 @@
 
+const noteDb = require('../../db/db.json')
 const fs = require('fs');
+const uuid = require('../../helper/uuid');
 const path = require('path');
 const express = require("express");
 
 const router = express.Router();
 
 router.get('/notes', (req,res) => {
-
     
 }) 
 
